ca9d602d34009e5aaa8e5601058145c3877ec3c1
miltondw <estradamilton2001@gmail.com>
2021-04-29 21:17:43 -0500

    Test con Hooks

--- src/Apjx.jsx
+++ src/Apjx.jsx
@@ -1,35 +1,53 @@
-import React, { Component } from 'react'
-import {
-    BrowserRouter as Router, Route, Switch
-  } from "react-router-dom";
-import './styles/App.scss'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles/App.scss";
 //images
-import imgbanner from './images/banner.webp'
-
+import imgbanner from "./images/banner.webp";
 
 //components
-import Banner from './components/Banner'
-import CourseGrid from './components/CourseGrid'
-import Form from './components/Form';
-import ContentCourse from './components/ContentCourse'
-import Error404 from './components/Error404'
-// import NoMatch from './components/NoMatch'
- 
-class Apjx extends Component {
+import Banner from "./components/organisms/Banner";
+import CourseGrid from "./components/organisms/CourseGrid";
+import Form from "./components/Pages/Form";
+import ContentCourse from "./components/organisms/ContentCourse";
+import Error404 from "./components/Pages/Error404";
+import MainMenu from "./components/Molecules/MainMenu";
+import History from "./components/Pages/History";
+import Users from './components/Hooks/HookTes'
+// import NoMatch from './components/Pages/NoMatch'
 
-    render() { 
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={()=><Banner title="Donde Empiezan todas las ideas" paragragh="No vale soñar si no ejecutas" img={imgbanner} />}/>
-                    <Route path="/cursos/:id" component={ContentCourse}/>
-                    <Route path="/cursos" component={CourseGrid}/>
-                    <Route path="/formulario" component={()=><Form title="Pide tu futuro ya!" />}/>
-                    <Route component={Error404}/>
-                </Switch>
-            </Router>
-        )
-    }
+class Apjx extends Component {
+  render() {
+    return (
+      <Router>
+        <MainMenu />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={() => (
+              <Banner
+                title="Donde Empiezan todas las ideas"
+                paragragh="No vale soñar si no ejecutas"
+                img={imgbanner}
+              />
+            )}
+          />
+          <Route path="/cursos/:id" component={ContentCourse} />
+          <Route path="/history" component={History} />
+          <Route path="/cursos" component={CourseGrid} />
+          <Route
+            path="/formulario"
+            component={() => <Form title="Pide tu futuro ya!" />}
+          />
+          <Route 
+            path="/users"
+            component={Users}
+          />
+          <Route component={Error404} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Apjx;
